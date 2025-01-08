b75c01e08e75648226aa14f3491070985b0b2805
naseefmw <60293453+naseefmw@users.noreply.github.com>
2025-01-08 13:16:11 +0530

    bookdetails done

--- src/app/bookdetails/page.js
+++ src/app/bookdetails/page.js
@@ -1,4 +1,42 @@
+import { gql } from '@apollo/client'
+import createApolloClient from '@/services/apollo-client'
+
+const BOOK_DETAILS = gql`
+  query BookDetails($id: ID!) {
+    bookById(id: $id) {
+      title
+      author
+      pub_date
+      isbn
+      rating
+      genre
+      image
+    }
+  }
+`
+async function getBookDetails(id) {
+  const client = createApolloClient()
+  const { data } = await client.query({
+    query: BOOK_DETAILS,
+    variables: { id: id },
+  })
+  return data.bookById
+}
+
 export default async function BookDetails({ searchParams }) {
   const { id } = await searchParams
-  return <div>book details of {id}</div>
+  const bookDetails = await getBookDetails(id)
+  return (
+    <div>
+      title: {bookDetails.title} <br />
+      author: {bookDetails.author} <br />
+      pub_date: {bookDetails.pub_date} <br />
+      isbn: {bookDetails.isbn} <br />
+      rating: {bookDetails.rating}
+      <br />
+      genre: {bookDetails.genre}
+      <br />
+      image: {bookDetails.image}
+    </div>
+  )
 }
