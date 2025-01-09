b038085fca16ce8800e5a624a87c169dcb38b02f
naseefmw <60293453+naseefmw@users.noreply.github.com>
2025-01-09 20:56:36 +0530

    delete button added

--- src/components/Home/BookItem.js
+++ src/components/Home/BookItem.js
@@ -1,5 +1,30 @@
+'use client'
 import Link from 'next/link'
+import { gql } from '@apollo/client'
+import createApolloClient from '@/services/apollo-client'
+import { Button } from 'react-bootstrap'
+
+const DELETE_BOOK = gql`
+  mutation DeleteBook($id: ID!) {
+    deleteBook(id: $id) {
+      title
+    }
+  }
+`
+
 export default function BookItem({ id, title, author, rating }) {
+  const client = createApolloClient()
+
+  async function onDelete(event) {
+    event.preventDefault()
+    const { data } = await client.mutate({
+      mutation: DELETE_BOOK,
+      variables: {
+        id: id,
+      },
+    })
+    console.log(data)
+  }
   return (
     <tr>
       <td>
@@ -14,7 +39,11 @@ export default function BookItem({ id, title, author, rating }) {
       </td>
       <td>{author}</td>
       <td>{rating}</td>
-      <td>delete</td>
+      <td>
+        <Button variant="danger" onClick={onDelete}>
+          Delete
+        </Button>
+      </td>
     </tr>
   )
 }
